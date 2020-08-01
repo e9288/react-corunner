f16039d5456427fc1d0c3492807fdaade833a4d4
react student <e9288@naver.com>
2020-08-01 10:47:09 +0900

    counter 컴포넌트 증/감 로직 - 통신 프로토타입 구현

--- src/components/Player.js
+++ src/components/Player.js
@@ -2,14 +2,14 @@ import {Counter} from "./Counter";
 import React from "react";
 
 export const Player = (props) => {
-  console.log(props);
+  // console.log(props);
   return (
     <div className="player">
       <span className='player-name'>
         <button className='remove-player' onClick={() => props.removePlayer(props.id)}> x </button>
         {props.name}
       </span>
-      <Counter score={props.score}></Counter>
+      <Counter score={props.score} changeScore={props.changeScore} id={props.id} />
     </div>
   );
 }
