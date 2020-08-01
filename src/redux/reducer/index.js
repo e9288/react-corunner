76a976c755696d445a93a498a120a1cbe7742ba0
react student <e9288@naver.com>
2020-08-01 18:23:19 +0900

    redux추가

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,8 +1,8 @@
 import {combineReducers} from "redux";
 import {productReducer} from "./productReducer";
 import {userReducer} from "./userReducer";
+import {playerReducer} from "./playerReducer";
 
 export const allReducers = combineReducers({
-  products: productReducer,
-  user: userReducer
+  playerReducer: playerReducer
 })
