242619f83551b57eed7728e006a12bdadfc0dce7
react student <e9288@naver.com>
2020-08-08 13:01:40 +0900

    CustomPlayer 생성 , 하이스코어 동적 클래스 바인딩

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import './App.css';
 import {Header} from './components/Header';
-import Player from './components/Player';
+import {CustomPlayer} from './components/CustomPlayer';
 import AddPlayerForm from "./components/AddPlayerForm";
 import {connect} from "react-redux";
+import * as _ from 'lodash';
+
 
 class App extends React.Component {
 
+  getHighScore() {
+    const highScoreObject = _.maxBy(this.props.players, 'score');
+    return highScoreObject.score === 0 ? '' : highScoreObject.score;
+  }
 
   handleChangeScore = (id, delta) => {
     // console.log('handleScore:', id,delta);
     this.setState(prevState => {
-      const players = [ ...prevState.players ];
+      const players = [...prevState.players];
       players.forEach(player => {
-        if(player.id === id) {
+        if (player.id === id) {
           player.score += delta;
         }
       });
@@ -24,14 +30,14 @@ class App extends React.Component {
   render() {
     return (
       <div className="scoreboard">
-        <Header players={this.props.players} />
+        <Header players={this.props.players}/>
 
         {
           this.props.players.map(player =>
-            <Player name={player.name} score={player.score} id={player.id} key={player.id}
-                    changeScore={this.handleChangeScore} />)
+            <CustomPlayer name={player.name} score={player.score} id={player.id} key={player.id}
+                          isHighScore={player.score === this.getHighScore()}></CustomPlayer>)
         }
-        <AddPlayerForm />
+        <AddPlayerForm/>
       </div>
     );
   }
